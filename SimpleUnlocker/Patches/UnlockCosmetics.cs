92a300795c84d1379f3a0dcb3a29fb6335fb1c7f
void <void@freewifi.club>
2025-08-14 21:54:50 +0200

    Create UnlockCosmetics.cs

---
+++ SimpleUnlocker/Patches/UnlockCosmetics.cs
@@ -0,0 +1,16 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace SimpleUnlocker.Patches;
+
+[HarmonyPatch(typeof(CustomizationOption), nameof(CustomizationOption.IsLocked), MethodType.Getter)]
+public class UnlockCosmetics {
+    public static bool Prefix(CustomizationOption __instance, ref bool __result) {
+        if (!SimpleUnlocker.UnlockCosmetics.Value) {
+            return true;
+        }
+        
+        __result = false;
+        return false;
+    }
+}
